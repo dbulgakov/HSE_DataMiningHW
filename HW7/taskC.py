e261a9d641c58e2ba3a82013c73050b7e9751222
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-10-17 02:38:06 +0300

    updated to write to file

--- HW7/taskC.py
+++ HW7/taskC.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*
-# Выведите все строки данного файла в обратном порядке.
-# Для этого считайте список всех строк при помощи метода readlines().
-# Последняя строка входного файла обязательно заканчивается символом '\n'.
-
 fin = open("input.txt")
 lines_list = fin.readlines()
-print("".join(list(reversed(lines_list))).strip("\n"))
+f_out = open("output.txt", "w")
+print("".join(list(reversed(lines_list))).strip("\n"), file=f_out)
+fin.close()
+f_out.close()
