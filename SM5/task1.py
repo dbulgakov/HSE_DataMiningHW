25c372e055cc5cee70a576096e93fd04f65af838
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-03 17:27:08 +0300

    Binary search (wrong)

---
+++ SM5/task1.py
@@ -0,0 +1,14 @@
+a = [1, 3, 3, 10, 13, 123, 1234]
+x = 123
+l = 0
+r = len(a) - 1
+while r - l > 1:
+    m = (r + 1) // 2
+    if x >= a[m]:
+        l = m
+    else:
+        r = m
+if x == a[l] or x == a[r]:
+    print("YES")
+else:
+    print("NO")
