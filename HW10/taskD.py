c65584e327263d4b05104466fe39f641034de767
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-17 00:52:58 +0300

    solved task D

---
+++ HW10/taskD.py
@@ -0,0 +1,23 @@
+def isInList(list, number):
+    a = 0
+    b = len(list)
+    while True:
+        if a == b:
+           return "NO"
+        middle = (a + b) // 2
+        if list[middle] == number:
+            return "YES"
+        if list[middle] < number:
+            a = middle + 1
+        else:
+            b = middle
+
+len_arr = list(map(int, input().strip().split()))
+input_numbers = list(map(int, input().strip().split()))
+input_queries = list(map(int, input().strip().split()))
+
+for number in input_queries:
+    print(isInList(input_numbers, number))
+
+
+
