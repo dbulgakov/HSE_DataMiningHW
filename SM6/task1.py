81c722fe96514fd8a18e6cad569c82da47511b6d
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-17 17:22:27 +0300

    pseudocode task 1

---
+++ SM6/task1.py
@@ -0,0 +1,14 @@
+# scob posled-ti stack
+
+list = list()
+x = input()
+for i in list:
+    if x == '(':
+        push(x)
+    else:
+        if pop() == '('
+            continue
+        else:
+            print("NO")
+if pop == null:
+    print("YES")
