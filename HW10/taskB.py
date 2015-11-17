7764c1ce221c402286c9f239c61e21303907ad81
Dmitry Bulgakov <dbulgakov@users.noreply.github.com>
2015-11-17 03:31:33 +0300

    solved task B

--- HW10/taskB.py
+++ HW10/taskB.py
@@ -1,17 +1,11 @@
 def findnumber(list, number):
-    a = 0
-    b = len(list) - 1
-    while(True):
-        middle = (a + b) // 2
-        if number < list[middle]:
-            b = middle
-        elif number > list[middle]:
-            a = middle
-        else:
-            return middle
+    c_num = list[0]
+    for i in list:
+        if abs(i - number) < abs (c_num - number):
+            c_num = i
+    return c_num
 
 tmp = input()
-input_numbers = map(int, input().strip().split())
-input_numbers = sorted(input_numbers)
+input_numbers = list(map(int, input().strip().split()))
 number_to_find = int(input().strip())
 print(findnumber(input_numbers, number_to_find))
